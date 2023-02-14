df9b3c131a626375222852ea083bec2ea8c980c7
Taft Barrott <tjbarrott@yahoo.com>
2023-02-13 19:29:27 -0600

    Add project files.

---
+++ MauiAppNet6/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiAppNet6;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MauiAppNet6/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿namespace MauiAppNet6;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+
+		var mergedDictionaries = Application.Current.Resources.MergedDictionaries;
+		foreach(var dict in mergedDictionaries)
+		{
+			if (dict.ContainsKey("Primary"))
+			{
+				Console.WriteLine("Primary was found in dictionary.");
+			}
+
+			foreach(var key in dict.Keys)
+			{
+				bool containsKey = dict.ContainsKey(key);
+
+				if (containsKey)
+				{
+					Console.WriteLine($"{key} was found in dictionary.");
+				}
+				else
+				{
+					Console.WriteLine($"{key} NOT found in dict.");
+				}
+			}
+		}
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
+
